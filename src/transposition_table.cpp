f143c824d50e5f8eb53f2d5a8df636c5293462fa
enfmarinho <eduardonestormarinho228@gmail.com>
2024-04-19 17:52:25 -0300

    Implementing tabulation table.

--- src/transposition_table.cpp
+++ src/transposition_table.cpp
@@ -4,3 +4,71 @@
  *  Licensed under the MIT License.
  *  See the LICENSE file in the project root for more information.
  */
+
+#include "transposition_table.h"
+#include "game_elements.h"
+#include "hashing.h"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <strings.h>
+
+IndexType TTEntry::relative_age(IndexType half_move_counter) const {
+  return half_move_counter - m_half_move_count;
+}
+
+int TTEntry::replace_factor(const IndexType &half_move_counter) const {
+  return int(m_depth) - int(relative_age(half_move_counter)) * 2;
+}
+
+void TTEntry::save(const HashType &key, const IndexType &depth,
+                   const Movement &movement, const WeightType &evaluation,
+                   const IndexType &half_move, const BoundType &bound) {
+  m_key = key;
+  m_depth = depth;
+  m_best_movement = movement;
+  m_evaluation = evaluation;
+  m_half_move_count = half_move;
+  m_bound = bound;
+}
+
+TTEntry *TranspositionTable::probe(const HashType &key, bool &found,
+                                   const IndexType &half_move_counter) {
+  HashType table_index = key & m_table_mask;
+  for (TTEntry &entry : m_table[table_index].entry) {
+    if (entry.key() == key) {
+      return found = true, &entry;
+    }
+  }
+
+  TTBucket *bucket = &m_table[table_index];
+  TTEntry *replace = &(bucket->entry[0]);
+  for (IndexType index = 0; index < bucket_size; ++index) {
+    if (replace->replace_factor(half_move_counter) >
+        bucket->entry[index].replace_factor(half_move_counter)) {
+      replace = &m_table[table_index].entry[index];
+    }
+  }
+  return found = false, replace;
+}
+
+void TranspositionTable::resize(size_t MB) {
+  m_table_size = MB * 1024 * 1024 / sizeof(TTBucket);
+  m_table_mask = m_table_size - 1;
+  m_table = std::make_unique<TTBucket[]>(m_table_size);
+  if (!m_table) {
+    std::cerr
+        << "Failed to allocated required memory for the transposition table"
+        << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+void TranspositionTable::clear() {
+  for (size_t index = 0; index < m_table_size; ++index) {
+    for (TTEntry &entry : m_table[index].entry) {
+      entry.reset();
+    }
+  }
+}
--- src/transposition_table.h
+++ src/transposition_table.h
@@ -9,16 +9,69 @@
 #define TRANSPOSITION_TABLE_H
 
 #include "game_elements.h"
+#include "hashing.h"
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+class TTEntry {
+public:
+  enum class BoundType : char {
+    Empty,
+    Exact,
+    LowerBound,
+    UpperBound,
+  };
+
+  HashType key() const { return m_key; }
+  IndexType depth() const { return m_depth; }
+  Movement movement() const { return m_best_movement; }
+  WeightType evaluation() const { return m_evaluation; }
+  BoundType bound() const { return m_bound; }
+  int relative_age(IndexType half_move_count) const;
+  int replace_factor(const IndexType &half_move_count) const;
+  void save(const HashType &key, const IndexType &depth,
+            const Movement &movement, const WeightType &evaluation,
+            const IndexType &half_move, const BoundType &bound);
+  void reset() { m_bound = BoundType::Empty; }
+
+private:
+  HashType m_key;           // 8 bytes
+  int8_t m_depth;           // 1 byte // TODO change to indextype
+  Movement m_best_movement; // 3 bytes
+  int16_t m_evaluation;     // 2 bytes // TODO change to WeightType
+  int8_t m_half_move_count; // 1 byte // TOOD check if it can be CounterType
+  BoundType m_bound = BoundType::Empty; // 1 byte
+};
 
 class TranspositionTable {
+  constexpr static IndexType bucket_size = 4;
+  struct TTBucket {
+    std::array<TTEntry, bucket_size> entry;
+  };
+
+  static_assert(sizeof(TTEntry) == 16, "TTEntry is not 16 bytes");
+  static_assert(sizeof(TTBucket) == 64, "TTBucket is not 64 bytes");
+
 public:
-  TranspositionTable() {}
-  // TODO
+  TranspositionTable() = default;
+  ~TranspositionTable() = default;
+  /*!
+   * Search the transposition table for the TTEntry with "key" and sets "found"
+   * to true if it was found, otherwise sets "found" to false and returns a
+   * pointer to the TTEntry with the least value, i.e. the one that should be
+   * replaced.
+   */
+  TTEntry *probe(const HashType &key, bool &found,
+                 const IndexType &half_move_count);
+  void resize(size_t MB);
+  void clear();
+
 private:
-  int m_key;
-  IndexType m_depth;
-  Movement m_movement;
-  WeightType m_evaluation;
+  size_t m_table_size{0};
+  size_t m_table_mask{0};
+  std::unique_ptr<TTBucket[]> m_table;
 };
 
 #endif // #ifndef TRANSPOSITION_TABLE_H
