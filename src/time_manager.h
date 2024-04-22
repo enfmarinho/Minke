63c2b983f8f4e904d8f3ef4d4f657fa8b07b1fd8
enfmarinho <eduardonestormarinho228@gmail.com>
2024-04-22 02:48:25 -0300

    Outlining time manager class.

---
+++ src/time_manager.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (c) 2024 Eduardo Marinho <eduardo.nestor.marinho@proton.me>
+ *
+ *  Licensed under the MIT License.
+ *  See the LICENSE file in the project root for more information.
+ */
+
+#ifndef TIME_MANAGER_H
+#define TIME_MANAGER_H
+
+#include <chrono>
+
+class TimeManager {
+  using TimeType = std::chrono::steady_clock::time_point;
+
+  void reset() { /* TODO  */ }
+
+  TimeType maximum() const { return m_maximum_time; }
+  TimeType optimum() const { return m_optimum_time; }
+  TimeType elapsed() const { /* TODO */ }
+
+private:
+  TimeType m_maximum_time;
+  TimeType m_optimum_time;
+  TimeType m_start_time;
+};
+
+#endif // #endif TIME_MANAGER_H
