d28445eb9a065ffc771461423551549a6d545af7
enfmarinho <eduardonestormarinho228@gmail.com>
2024-05-30 19:56:48 -0300

    Adding HashType definition.

--- src/game_elements.hpp
+++ src/game_elements.hpp
@@ -14,6 +14,7 @@
 using IndexType = int; // TODO change to int8_t
 using CounterType = int;
 using WeightType = int32_t; // TODO change to int16_t
+using HashType = uint64_t;
 
 constexpr CounterType NumberOfPieces = 6;
 constexpr IndexType BoardHeight = 8;
