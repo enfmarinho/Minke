49eeb053f7cff1e0eb605bfd7e3af79af9a97cfe
enfmarinho <eduardonestormarinho228@gmail.com>
2024-06-26 00:43:19 -0300

    Fixing how the search save entries on the TT.

--- src/search.cpp
+++ src/search.cpp
@@ -13,12 +13,14 @@
 #include "position.h"
 #include "thread.h"
 #include "tt.h"
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 
 void search::search(GameState &game_state, Thread &thread) {
   iterative_deepening<true>(game_state, thread);
 }
+
 void search::perft(GameState &game_state, Thread &thread) {
   iterative_deepening<false>(game_state, thread);
 }
@@ -69,32 +71,55 @@ WeightType search::alpha_beta_search(WeightType alpha, WeightType beta,
 
   if (thread.should_stop())
     return ScoreNone;
-  if (depth_ply == 0)
-    return eval::evaluate(
+  if (depth_ply == 0) {
+    // WeightType eval = game_state.eval(); // TODO do a quiesce search
+    WeightType eval = eval::evaluate(
         game_state.position()); // TODO STUB while nn is incomplete
-  // return game_state.eval(); // TODO do a quiesce search
+    TTEntry::BoundType bound = TTEntry::BoundType::Exact;
+    if (eval <= alpha)
+      bound = TTEntry::BoundType::LowerBound;
+    else if (eval >= beta)
+      bound = TTEntry::BoundType::UpperBound;
+    entry->save(game_state.position().get_hash(), 0, move_none, eval,
+                game_state.position().get_hash(), bound);
+    return eval;
+  }
 
   Move best_move = move_none;
-  if (found)
+  WeightType best_eval = ScoreNone;
+  if (found) {
     best_move = entry->best_move();
+    best_eval = entry->evaluation();
+  }
   MoveList move_list(game_state, best_move);
   while (!move_list.empty()) {
     Move move = move_list.next_move();
     if (!game_state.make_move(move))
       continue;
-    WeightType score =
+    WeightType eval =
         alpha_beta_search(-beta, -alpha, depth_ply - 1, game_state, thread);
     game_state.undo_move();
-    if (score >= beta) {
-      game_state.increment_history(move, depth_ply);
-      return beta;
-    } else if (score > alpha)
-      alpha = score, best_move = move;
-  }
-  if (!thread.should_stop())
-    entry->save(game_state.position().get_hash(), depth_ply, best_move, alpha,
-                game_state.position().get_half_move_counter(),
-                TTEntry::BoundType::Empty); // TODO bound is just a STUB
 
+    if (eval > best_eval) {
+      best_eval = eval;
+      best_move = move;
+    } else if (eval >= beta) {
+      game_state.increment_history(move, depth_ply);
+      break;
+    } else if (eval > alpha)
+      alpha = eval;
+  }
+
+  if (!thread.should_stop()) {
+    TTEntry::BoundType bound = TTEntry::BoundType::Exact;
+    if (best_eval <= alpha)
+      bound = TTEntry::BoundType::LowerBound;
+    else if (best_eval >= beta)
+      bound = TTEntry::BoundType::UpperBound;
+
+    entry->save(game_state.position().get_hash(), depth_ply, best_move,
+                best_eval, game_state.position().get_half_move_counter(),
+                bound);
+  }
   return alpha;
 }
