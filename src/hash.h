dc293fd84e3f46015d7c5f0c7d32c5eb91bddbb5
enfmarinho <eduardonestormarinho228@gmail.com>
2024-08-28 19:59:31 -0300

    Adding Stockfish's pseudo-random number generator.

--- src/hash.h
+++ src/hash.h
@@ -10,6 +10,7 @@
 
 #include "game_elements.h"
 #include "position.h"
+#include <cassert>
 
 namespace zobrist {
 HashType hash(const Position &position);
@@ -25,5 +26,43 @@ constexpr static int BlackQueenSideCastlingRightsIndex = 771;
 constexpr static int EnPassantStarterIndex = 772;
 constexpr static int BlackTurnIndex = 780;
 }; // namespace zobrist
+
+//==== Taken from stockfish
+// xorshift64star Pseudo-Random Number Generator
+// This class is based on original code written and dedicated
+// to the public domain by Sebastiano Vigna (2014).
+// It has the following characteristics:
+//
+//  -  Outputs 64-bit numbers
+//  -  Passes Dieharder and SmallCrush test batteries
+//  -  Does not require warm-up, no zeroland to escape
+//  -  Internal state is a single 64-bit integer
+//  -  Period is 2^64 - 1
+//  -  Speed: 1.60 ns/call (Core i7 @3.40GHz)
+//
+// For further analysis see
+//   <http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
+
+class PRNG {
+
+  HashType s;
+
+  HashType rand64() {
+
+    s ^= s >> 12, s ^= s << 25, s ^= s >> 27;
+    return s * 2685821657736338717LL;
+  }
+
+public:
+  PRNG(HashType seed) : s(seed) { assert(seed); }
+
+  template <typename T> T rand() { return T(rand64()); }
+
+  // Special generator used to fast init magic numbers.
+  // Output values only have 1/8th of their bits set on average.
+  template <typename T> T sparse_rand() {
+    return T(rand64() & rand64() & rand64());
+  }
+};
 
 #endif // #ifndef HASH_H
