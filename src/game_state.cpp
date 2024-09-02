9d9a5fb758460bc4f95ead910ef810c7ba7ca550
enfmarinho <eduardonestormarinho228@gmail.com>
2024-09-02 09:02:13 -0300

    Fixing bug on NNUE input updates.

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -26,6 +26,7 @@ bool GameState::make_move(const Move &move) {
   m_net.push();
   m_last_move = move;
 
+  Piece pawn_promoted_to = Piece::None;
   const Square &moved = top().consult(move.to);
   m_net.add_feature(moved, move.to);
   m_net.remove_feature(moved, move.from);
@@ -37,9 +38,13 @@ bool GameState::make_move(const Move &move) {
         m_position_stack[m_position_stack.size() - 2].consult(move.to);
     m_net.remove_feature(captured, move.to);
   } else if (move.move_type == MoveType::EnPassant) {
+    const IndexType pawn_offset =
+        moved.player == Player::White ? offsets::North : offsets::South;
     const Player adversary =
-        (moved.player == Player::White ? Player::Black : Player::White);
-    m_net.remove_feature({Piece::Pawn, adversary}, move.to);
+        moved.player == Player::White ? Player::Black : Player::White;
+    m_net.remove_feature(
+        {Piece::Pawn, adversary},
+        PiecePlacement(move.to.file() - pawn_offset, move.to.rank()));
   } else if (move.move_type == MoveType::KingSideCastling) {
     const IndexType file = (moved.player == Player::White ? 0 : 7);
     m_net.add_feature({Piece::Rook, moved.player}, PiecePlacement(file, 5));
@@ -48,6 +53,24 @@ bool GameState::make_move(const Move &move) {
     const IndexType file = (moved.player == Player::White ? 0 : 7);
     m_net.add_feature({Piece::Rook, moved.player}, PiecePlacement(file, 3));
     m_net.remove_feature({Piece::Rook, moved.player}, PiecePlacement(file, 0));
+  } else if (move.move_type == MoveType::PawnPromotionQueen) {
+    pawn_promoted_to = Piece::Queen;
+  } else if (move.move_type == MoveType::PawnPromotionKnight) {
+    pawn_promoted_to = Piece::Knight;
+  } else if (move.move_type == MoveType::PawnPromotionRook) {
+    pawn_promoted_to = Piece::Rook;
+  } else if (move.move_type == MoveType::PawnPromotionBishop) {
+    pawn_promoted_to = Piece::Bishop;
+  }
+
+  if (pawn_promoted_to != Piece::None) {
+    m_net.remove_feature(moved, move.to); // Remove just added feature
+    m_net.add_feature({pawn_promoted_to, moved.player}, move.to);
+    const Square &to_before_move =
+        m_position_stack[m_position_stack.size() - 2].consult(move.to);
+    if (to_before_move.piece != Piece::None) { // Capture happened
+      m_net.remove_feature(to_before_move, move.to);
+    }
   }
 
   return true;
