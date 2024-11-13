c2f7cc951594035a5ab0921af78b5dd2abaf5f81
enfmarinho <eduardonestormarinho228@gmail.com>
2024-11-13 14:39:22 -0300

    Removing error with past commit.

--- src/time_manager.cpp
+++ src/time_manager.cpp
@@ -11,7 +11,6 @@
 
 TimeManager::TimeManager() : m_time_set{false}, m_can_stop{false} {}
 
-void TimeManager::init(CounterType inc, CounterType time, CounterType movestogo, CounterType movetime, bool infinite) {
 void TimeManager::init(const Position &position, CounterType inc, CounterType time, CounterType movestogo,
                        CounterType movetime, bool infinite) {
     m_can_stop = false;
