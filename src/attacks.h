9ab4f7a8453d2db130ef8c647fd078274ab7731d
enfmarinho <eduardonestormarinho228@gmail.com>
2025-02-25 20:44:38 -0300

    Adding attacks files.

---
+++ src/attacks.h
@@ -0,0 +1,12 @@
+
+/*
+ *  Copyright (c) 2024 Eduardo Marinho <eduardo.nestor.marinho@proton.me>
+ *
+ *  Licensed under the MIT License.
+ *  See the LICENSE file in the project root for more information.
+ */
+
+#ifndef ATTACKS_H
+#define ATTACKS_H
+
+#endif // #ifndef ATTACKS_H
