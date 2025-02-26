c85f87ba090cac7ce216d3a48d00ac010f280358
enfmarinho <eduardonestormarinho228@gmail.com>
2025-02-25 21:25:39 -0300

    Adding init files

---
+++ src/init.h
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2024 Eduardo Marinho <eduardo.nestor.marinho@proton.me>
+ *
+ *  Licensed under the MIT License.
+ *  See the LICENSE file in the project root for more information.
+ */
+
+#ifndef INIT_H
+#define INIT_H
+
+void init_all();
+
+void init_network_parameters();
+
+void init_hash_keys();
+
+void init_attack_tables();
+
+#endif // #ifndef INIT_H
