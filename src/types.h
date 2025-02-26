08fffe763f0532aa65eb9e82d41a79464f120b71
enfmarinho <eduardonestormarinho228@gmail.com>
2025-02-25 21:24:09 -0300

    Refactoring: changing types.

--- src/types.h
+++ src/types.h
@@ -11,12 +11,83 @@
 #include <chrono>
 #include <cstdint>
 
+// clang-format off
+enum Square : int {
+    a1, b1, c1, d1, e1, f1, g1, h1,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    NoSquare
+};
+// clang-format on
+
+constexpr int ColorOffset = 6;
+
+enum Color : int {
+    White,
+    Black,
+};
+
+enum PieceType : int {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+    None
+};
+
+enum Piece : int {
+    WhitePawn,
+    WhiteKnight,
+    WhiteBishop,
+    WhiteRook,
+    WhiteQueen,
+    WhiteKing,
+
+    BlackPawn,
+    BlackKnight,
+    BlackBishop,
+    BlackRook,
+    BlackQueen,
+    BlackKing,
+
+    Empty
+};
+
+enum CastlingRights : int {
+    WhiteShortCastle = 1,
+    WhiteLongCastle = 2,
+    BlackShortCastle = 4,
+    BlackLongCastle = 8
+};
+
+enum Direction : int {
+    North = 8,
+    South = -8,
+    West = -1,
+    East = 1,
+    NorthEast = North + East,
+    NorthWest = North + West,
+    SouthEast = South + East,
+    SouthWest = South + West,
+
+    DoubleNorth = 2 * North,
+    DoubleSouth = 2 * South,
+};
+
+using HashType = uint64_t;
 using IndexType = int8_t;
 using CounterType = int;
 using WeightType = int32_t; // TODO change to int16_t
-using HashType = uint64_t;
 using TimeType = std::chrono::milliseconds::rep;
 using TimePoint = std::chrono::steady_clock::time_point;
+using Bitboard = uint64_t;
 
 constexpr WeightType MateScore = 100'000;
 constexpr WeightType MaxScore = 200'000;
@@ -24,142 +95,22 @@ constexpr CounterType NumberOfPieces = 6;
 constexpr IndexType BoardHeight = 8;
 constexpr IndexType BoardWidth = 8;
 
-using PieceSquareTable = WeightType[BoardHeight][BoardWidth];
-using PieceSquareTablePointer = const PieceSquareTable *;
-
-constexpr auto StartFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-constexpr int MaxMoves = 255;
+constexpr inline auto StartFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+constexpr int MaxMoves = 256;
 constexpr int HalfMovesPerMatch = 2048;
 constexpr int MaxSearchDepth = 128;
-
-namespace offsets {
-constexpr IndexType North = 1;
-constexpr IndexType East = 16;
-constexpr IndexType South = -North;
-constexpr IndexType West = -East;
-constexpr IndexType Northeast = North + East;
-constexpr IndexType Northwest = North + West;
-constexpr IndexType Southeast = South + East;
-constexpr IndexType Southwest = South + West;
-
-constexpr IndexType Knight[8] = {East * 2 + North, East * 2 + South, South * 2 + East, South * 2 + West,
-                                 West * 2 + South, West * 2 + North, North * 2 + West, North * 2 + East};
-constexpr IndexType AllDirections[8] = {East, West, South, North, Southeast, Southwest, Northeast, Northwest};
-
-constexpr IndexType Sliders[3][8] = {
-    {Southeast, Southwest, Northeast, Northwest},                          // Bishop
-    {North, South, East, West},                                            // Rook
-    {East, West, South, North, Southeast, Southwest, Northeast, Northwest} // Queen
-};
-
-} // namespace offsets
 
 inline TimeType now() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch())
         .count();
 }
 
-#define piece_index(piece) static_cast<IndexType>(piece)
-enum class Piece : char {
-    Pawn = 0,
-    Knight,
-    Bishop,
-    Rook,
-    Queen,
-    King,
-    None,
+struct BoardState {
+    PieceType captured;
+    int fifty_move_clock;
+    int ply_after_reset;
+    uint8_t castle_rights;
+    Square en_passant;
 };
-
-enum class Player : char {
-    White = 1,
-    Black = -1,
-    None = 0,
-};
-
-struct CastlingRights {
-    bool king_side;
-    bool queen_side;
-    CastlingRights() : king_side(false), queen_side(false) {}
-};
-
-enum class MoveType : char {
-    KingSideCastling,
-    QueenSideCastling,
-    EnPassant,
-    PawnPromotionKnight,
-    PawnPromotionBishop,
-    PawnPromotionRook,
-    PawnPromotionQueen,
-    Regular,
-    Capture,
-};
-
-struct PiecePlacement {
-    constexpr static const IndexType padding = 4;
-    IndexType file() const { return m_index & 7; }
-    IndexType rank() const { return m_index >> padding; }
-    IndexType index() const { return m_index; }
-    IndexType &index() { return m_index; }
-    bool out_of_bounds() const { return m_index & 0x88; }
-    PiecePlacement mirrored() const {
-        return PiecePlacement(m_index ^ 119); // 119 is (7 << 4) + 7
-    }
-    PiecePlacement() {};
-    PiecePlacement(IndexType index) : m_index(index) {};
-    PiecePlacement(const IndexType &file, const IndexType &rank) { m_index = file + (rank << padding); }
-    friend bool operator==(const PiecePlacement &lhs, const PiecePlacement &rhs) { return lhs.m_index == rhs.m_index; }
-
-  private:
-    IndexType m_index; // file is stored in the 4 least significant bits, while
-                       // rank is store in the others 4
-};
-static_assert(sizeof(PiecePlacement) == 1);
-
-struct Square {
-    Piece piece;
-    Player player;
-    Square() {
-        piece = Piece::None;
-        player = Player::None;
-    }
-    Square(Piece piece, Player player) : piece(piece), player(player) {}
-    friend bool operator==(const Square &lhs, const Square &rhs) {
-        return lhs.piece == rhs.piece && lhs.player == rhs.player;
-    }
-};
-const Square EmptySquare = Square(Piece::None, Player::None);
-static_assert(sizeof(Square) == 2);
-
-struct PastMove {
-    Move movement;
-    Square captured;
-    IndexType past_en_passant;
-    CounterType past_fifty_move_counter;
-    CastlingRights past_white_castling_rights;
-    CastlingRights past_black_castling_rights;
-    PastMove(Move movement, Square captured, IndexType past_en_passant, CounterType past_fifty_move_counter,
-             CastlingRights past_white_castling_rights, CastlingRights past_black_castling_rights)
-        : movement(movement),
-          captured(captured),
-          past_en_passant(past_en_passant),
-          past_fifty_move_counter(past_fifty_move_counter),
-          past_white_castling_rights(past_white_castling_rights),
-          past_black_castling_rights(past_black_castling_rights) {}
-    PastMove() = default;
-};
-
-// clang-format off
-// Needed for index conversion, NNUE trainers uses 0 = a8, while my board uses 0 = a1
-constexpr IndexType MailboxToStandardNNUE[0x80] = {
-    56, 48, 40, 32, 24, 16,  8, 0, 100, 100, 100, 100, 100, 100, 100, 100,
-    57, 49, 41, 33, 25, 17,  9, 1, 100, 100, 100, 100, 100, 100, 100, 100,
-    58, 50, 42, 34, 26, 18, 10, 2, 100, 100, 100, 100, 100, 100, 100, 100,
-    59, 51, 43, 35, 27, 19, 11, 3, 100, 100, 100, 100, 100, 100, 100, 100,
-    60, 52, 44, 36, 28, 20, 12, 4, 100, 100, 100, 100, 100, 100, 100, 100,
-    61, 53, 45, 37, 29, 21, 13, 5, 100, 100, 100, 100, 100, 100, 100, 100,
-    62, 54, 46, 38, 30, 22, 14, 6, 100, 100, 100, 100, 100, 100, 100, 100,
-    63, 55, 47, 39, 31, 23, 15, 7, 100, 100, 100, 100, 100, 100, 100, 100,
-};
-// clang-format on
 
 #endif // #ifndef GAME_ELEMENTS_H
