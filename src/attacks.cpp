a9b200b2dbb75bd9cccf4ea13fb4765310608d00
enfmarinho <eduardonestormarinho228@gmail.com>
2025-02-27 16:48:10 -0300

    Implementing generate attacks functions

--- src/attacks.cpp
+++ src/attacks.cpp
@@ -72,3 +72,137 @@ Bitboard generate_rook_mask(Square sq) {
     return mask;
 }
 
+Bitboard generate_pawn_attack(Square sq, Color color) {
+    Bitboard attacks = 0ULL;
+    Bitboard board = 0ULL;
+    set_bit(board, sq);
+
+    int forward_offset;
+    if (color == White)
+        forward_offset = North;
+    else
+        forward_offset = South;
+
+    if (board & not_a_file)
+        attacks |= shift(board, forward_offset + West);
+    if (board & not_h_file)
+        attacks |= shift(board, forward_offset + East);
+
+    return attacks;
+}
+
+Bitboard generate_knight_attack(Square sq) {
+    Bitboard attacks = 0ULL;
+    Bitboard board = 0ULL;
+    set_bit(board, sq);
+
+    if (board & not_a_file & not_1_2_rank)
+        attacks |= shift(board, 2 * South + West);
+    if (board & not_h_file & not_1_2_rank)
+        attacks |= shift(board, 2 * South + East);
+    if (board & not_a_file & not_7_8_rank)
+        attacks |= shift(board, 2 * North + West);
+    if (board & not_h_file & not_7_8_rank)
+        attacks |= shift(board, 2 * North + East);
+    if (board & not_ab_file & not_1_rank)
+        attacks |= shift(board, 2 * West + South);
+    if (board & not_ab_file & not_8_rank)
+        attacks |= shift(board, 2 * West + North);
+    if (board & not_hg_file & not_1_rank)
+        attacks |= shift(board, 2 * East + South);
+    if (board & not_hg_file & not_8_rank)
+        attacks |= shift(board, 2 * East + North);
+
+    return attacks;
+}
+
+Bitboard generate_bishop_attack(Square sq, const Bitboard& blockers) {
+    Bitboard attacks = 0ULL;
+    Bitboard board = 0ULL;
+    set_bit(board, sq);
+
+    for (Bitboard board_cp = board; board_cp & not_a_file & not_1_rank;) {
+        board_cp = shift(board_cp, SouthWest);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_h_file & not_1_rank;) {
+        board_cp = shift(board_cp, SouthEast);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_a_file & not_8_rank;) {
+        board_cp = shift(board_cp, NorthWest);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_h_file & not_8_rank;) {
+        board_cp = shift(board_cp, NorthEast);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+
+    return attacks;
+}
+
+Bitboard generate_rook_attack(Square sq, const Bitboard& blockers) {
+    Bitboard attacks = 0ULL;
+    Bitboard board = 0ULL;
+    set_bit(board, sq);
+
+    for (Bitboard board_cp = board; board_cp & not_8_rank;) {
+        board_cp = shift(board_cp, North);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_1_rank;) {
+        board_cp = shift(board_cp, South);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_a_file;) {
+        board_cp = shift(board_cp, West);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+    for (Bitboard board_cp = board; board_cp & not_h_file;) {
+        board_cp = shift(board_cp, East);
+        attacks |= board_cp;
+        if (board_cp & blockers)
+            break;
+    }
+
+    return attacks;
+}
+
+Bitboard generate_king_attack(Square sq) {
+    Bitboard attacks = 0ULL;
+    Bitboard board = 0ULL;
+    set_bit(board, sq);
+
+    if (board & not_a_file)
+        attacks |= shift(board, West);
+    if (board & not_h_file)
+        attacks |= shift(board, East);
+    if (board & not_1_rank)
+        attacks |= shift(board, South);
+    if (board & not_8_rank)
+        attacks |= shift(board, North);
+    if (board & not_a_file & not_1_rank)
+        attacks |= shift(board, SouthWest);
+    if (board & not_a_file & not_8_rank)
+        attacks |= shift(board, NorthWest);
+    if (board & not_h_file & not_1_rank)
+        attacks |= shift(board, SouthEast);
+    if (board & not_h_file & not_8_rank)
+        attacks |= shift(board, NorthEast);
+
+    return attacks;
+}
