cf2670cc0736f1cd89ff4b9898c5a97830a953b8
enfmarinho <eduardonestormarinho228@gmail.com>
2025-03-01 18:26:09 -0300

    Adding getters for piece attacks

--- src/attacks.h
+++ src/attacks.h
@@ -8,6 +8,8 @@
 #ifndef ATTACKS_H
 #define ATTACKS_H
 
+#include <cassert>
+
 #include "types.h"
 
 const Bitboard not_a_file = ~FileMasks[0];
@@ -58,6 +60,41 @@ inline Bitboard get_rook_attacks(const Square& sq, const Bitboard& occupancy) {
 
 inline Bitboard get_queen_attacks(const Square& sq, const Bitboard& occupancy) {
     return get_rook_attacks(sq, occupancy) | get_bishop_attacks(sq, occupancy);
+}
+
+template <PieceType piece_type>
+inline Bitboard get_piece_attacks(const Square& sq, const Bitboard& occupancy) {
+    assert(piece_type >= Knight && piece_type <= King);
+
+    if constexpr (piece_type == Knight)
+        return KnightAttacks[sq];
+    else if constexpr (piece_type == Bishop)
+        return get_bishop_attacks(sq, occupancy);
+    else if constexpr (piece_type == Rook)
+        return get_rook_attacks(sq, occupancy);
+    else if constexpr (piece_type == Queen)
+        return get_queen_attacks(sq, occupancy);
+    else if constexpr (piece_type == King)
+        return KingAttacks[sq];
+}
+
+inline Bitboard get_piece_attacks(const Square& sq, const Bitboard& occupancy, PieceType piece_type) {
+    assert(piece_type >= Knight && piece_type <= King);
+
+    switch (piece_type) {
+        case Knight:
+            return KnightAttacks[sq];
+        case Bishop:
+            return get_bishop_attacks(sq, occupancy);
+        case Rook:
+            return get_rook_attacks(sq, occupancy);
+        case Queen:
+            return get_queen_attacks(sq, occupancy);
+        case King:
+            return KingAttacks[sq];
+        default:
+            __builtin_unreachable();
+    }
 }
 
 #endif // #ifndef ATTACKS_H
