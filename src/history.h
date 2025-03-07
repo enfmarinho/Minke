7d4ac2ed0ef47217e3cb8d1194b393d06fa8d9ba
enfmarinho <eduardonestormarinho228@gmail.com>
2025-03-07 01:22:09 -0300

    First History Heuristic implementation

---
+++ src/history.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2024 Eduardo Marinho <eduardo.nestor.marinho@proton.me>
+ *
+ *  Licensed under the MIT License.
+ *  See the LICENSE file in the project root for more information.
+ */
+
+#ifndef HISTORY_H
+#define HISTORY_H
+
+#include <cassert>
+#include <cstring>
+
+#include "move.h"
+#include "types.h"
+
+class History {
+  public:
+    inline History() { init(); }
+    ~History() = default;
+
+    inline void init() { std::memset(search_history_table, 0, sizeof(search_history_table)); };
+
+    inline void update(const Color &stm, const Move &move, int depth) {
+        search_history_table[stm][move.from_and_to()] = depth * depth;
+    }
+
+    inline int consult(const Color stm, const Move &move) const {
+        return search_history_table[stm][move.from_and_to()];
+    }
+
+  private:
+    int search_history_table[ColorNB][64 * 64];
+};
+#endif // #ifndef HISTORY_H
