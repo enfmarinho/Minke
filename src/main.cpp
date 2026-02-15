99595f400b0d90ac048c76fbbc78bc16a5d70bdc
enfmarinho <eduardonestormarinho228@gmail.com>
2026-02-15 02:55:13 -0300

    Refactor cli parser Bench: 3602349

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,34 @@
 
 #include <cstdlib>
 
+#include "datagen/datagen.h"
 #include "init.h"
 #include "uci.h"
 
 int main(int argc, char *argv[]) {
     init_all();
-    UCI uci(argc, argv);
-    uci.loop();
+    if (argc > 1 && std::string(argv[1]) == "bench") {
+        int depth = EngineOptions::BENCH_DEPTH;
+        if (argc > 2)
+            depth = std::stoi(argv[2]);
+
+        UCI uci;
+        uci.bench(depth);
+    } else if (argc > 1 && std::string(argv[1]) == "datagen") {
+        if (argc != 4) {
+            std::cerr << "usage: " << argv[0] << " datagen <threads> <output_directory>\n";
+            return EXIT_FAILURE;
+        }
+
+        int concurrency = std::stoi(argv[2]);
+        std::string directory = std::string(argv[3]);
+
+        DatagenEngine dt_engine;
+        dt_engine.datagen_loop(concurrency, EngineOptions::HASH_DEFAULT, directory);
+    } else {
+        UCI uci;
+        uci.loop();
+    }
+
     return EXIT_SUCCESS;
 }
