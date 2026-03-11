b7375c93129b803b360959848eb04946caa7f2e3
enfmarinho <eduardomarinho@pm.me>
2026-03-11 14:42:46 -0300

    Update datagen config Bench: 2385775

--- src/datagen/datagen.h
+++ src/datagen/datagen.h
@@ -36,11 +36,11 @@ class DatagenThread {
   private:
     static constexpr int VERIFICATION_MAX_SCORE = 800;
     static constexpr int VERIFICATION_SOFT_NODE_LIMIT = 80'000;
-    static constexpr int VERIFICATION_HARD_NODE_LIMIT = 500'000;
+    static constexpr int VERIFICATION_HARD_NODE_LIMIT = 200'000;
     static constexpr int VERIFICATION_MAX_DEPTH = 14;
 
-    static constexpr int SOFT_NODE_LIMIT = 25'000;
-    static constexpr int HARD_NODE_LIMIT = 100'000;
+    static constexpr int SOFT_NODE_LIMIT = 20'000;
+    static constexpr int HARD_NODE_LIMIT = 40'000;
 
     static constexpr int WIN_ADJ_PLY = 4;
     static constexpr int DRAW_ADJ_PLY = 12;
