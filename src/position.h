efaca635f8b9568d9656204c25002c8696758da5
enfmarinho <eduardonestormarinho228@gmail.com>
2026-05-07 00:21:01 -0300

    Make NNUE compatible with a king bucketed net

--- src/position.h
+++ src/position.h
@@ -61,7 +61,7 @@ class Position {
         return get_piece_bb(KNIGHT) || get_piece_bb(BISHOP) || get_piece_bb(ROOK) || get_piece_bb(QUEEN);
     }
     inline bool draw() { return insufficient_material() || repetition() || fifty_move_draw(); }
-    inline ScoreType eval() { return m_nnue.eval(m_stm); }
+    inline ScoreType eval() { return m_nnue.eval(*this); }
 
     int legal_move_amount();
     bool no_legal_moves();
