191e30265a2e6f9197ab559c90c58248def97638
enfmarinho <eduardonestormarinho228@gmail.com>
2026-07-21 03:03:52 -0300

    Fix scalar build Bench: 5070320

--- src/eval/nnue.cpp
+++ src/eval/nnue.cpp
@@ -110,44 +110,44 @@ void NNUE::activate_ft(std::span<const int16_t, L1_SIZE> stm_acc, std::span<cons
                        std::span<uint8_t, L1_SIZE> outputs) {
     constexpr size_t PAIR_COUNT = L1_SIZE / 2;
     const auto pov_activate = [&](std::span<const int16_t, L1_SIZE> acc, int output_offset) {
-        if constexpr (USE_SIMD) {
-            using namespace simd;
+#if USE_SIMD
+        using namespace simd;
 
-            vepi16 zero = zero_i16();
-            vepi16 one = set_i16(QA);
+        vepi16 zero = zero_i16();
+        vepi16 one = set_i16(QA);
 
-            for (size_t idx = 0; idx < PAIR_COUNT; idx += 4 * CHUNK_SIZE_16BIT) {
-                vepi16 i0_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 0]), zero, one);
-                vepi16 i1_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 1]), zero, one);
-                vepi16 i2_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 2]), zero, one);
-                vepi16 i3_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 3]), zero, one);
+        for (size_t idx = 0; idx < PAIR_COUNT; idx += 4 * CHUNK_SIZE_16BIT) {
+            vepi16 i0_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 0]), zero, one);
+            vepi16 i1_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 1]), zero, one);
+            vepi16 i2_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 2]), zero, one);
+            vepi16 i3_left = clamp_i16(load_i16(&acc[idx + CHUNK_SIZE_16BIT * 3]), zero, one);
 
-                vepi16 i0_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 0]), zero, one);
-                vepi16 i1_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 1]), zero, one);
-                vepi16 i2_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 2]), zero, one);
-                vepi16 i3_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 3]), zero, one);
+            vepi16 i0_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 0]), zero, one);
+            vepi16 i1_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 1]), zero, one);
+            vepi16 i2_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 2]), zero, one);
+            vepi16 i3_right = clamp_i16(load_i16(&acc[idx + PAIR_COUNT + CHUNK_SIZE_16BIT * 3]), zero, one);
 
-                vepi16 pw0 = mulhi_i16(shiftleft_i16(i0_left, FT_SCALE_BITS), i0_right);
-                vepi16 pw1 = mulhi_i16(shiftleft_i16(i1_left, FT_SCALE_BITS), i1_right);
-                vepi16 pw2 = mulhi_i16(shiftleft_i16(i2_left, FT_SCALE_BITS), i2_right);
-                vepi16 pw3 = mulhi_i16(shiftleft_i16(i3_left, FT_SCALE_BITS), i3_right);
+            vepi16 pw0 = mulhi_i16(shiftleft_i16(i0_left, FT_SCALE_BITS), i0_right);
+            vepi16 pw1 = mulhi_i16(shiftleft_i16(i1_left, FT_SCALE_BITS), i1_right);
+            vepi16 pw2 = mulhi_i16(shiftleft_i16(i2_left, FT_SCALE_BITS), i2_right);
+            vepi16 pw3 = mulhi_i16(shiftleft_i16(i3_left, FT_SCALE_BITS), i3_right);
 
-                vepu8 packed0 = packus_i16(pw0, pw1);
-                vepu8 packed1 = packus_i16(pw2, pw3);
+            vepu8 packed0 = packus_i16(pw0, pw1);
+            vepu8 packed1 = packus_i16(pw2, pw3);
 
-                store_u8(&outputs[output_offset + idx + CHUNK_SIZE_8BIT * 0], packed0);
-                store_u8(&outputs[output_offset + idx + CHUNK_SIZE_8BIT * 1], packed1);
-            }
-
-        } else {
-            for (int i = 0; i < PAIR_COUNT; ++i) {
-                int32_t i0_left = std::clamp<int16_t>(acc[i], 0, QA);
-                int32_t i0_right = std::clamp<int16_t>(acc[i + PAIR_COUNT], 0, QA);
-
-                // simulate mulhi
-                outputs[i + output_offset] = ((i0_left << FT_SCALE_BITS) * i0_right) >> 16;
-            }
+            store_u8(&outputs[output_offset + idx + CHUNK_SIZE_8BIT * 0], packed0);
+            store_u8(&outputs[output_offset + idx + CHUNK_SIZE_8BIT * 1], packed1);
         }
+
+#else
+        for (int i = 0; i < PAIR_COUNT; ++i) {
+            int32_t i0_left = std::clamp<int16_t>(acc[i], 0, QA);
+            int32_t i0_right = std::clamp<int16_t>(acc[i + PAIR_COUNT], 0, QA);
+
+            // simulate mulhi
+            outputs[i + output_offset] = ((i0_left << FT_SCALE_BITS) * i0_right) >> 16;
+        }
+#endif
     };
 
     pov_activate(stm_acc, 0);
@@ -157,72 +157,72 @@ void NNUE::activate_ft(std::span<const int16_t, L1_SIZE> stm_acc, std::span<cons
 void NNUE::propagate_l1(int bucket, std::span<const uint8_t, L1_SIZE> inputs, std::span<int32_t, L2_SIZE> outputs) {
     constexpr int shift = 8;
 
-    if constexpr (USE_SIMD) {
-        using namespace simd;
+#if USE_SIMD
+    using namespace simd;
 
-        const int32_t *packed_input = reinterpret_cast<const int32_t *>(inputs.data());
+    const int32_t *packed_input = reinterpret_cast<const int32_t *>(inputs.data());
 
-        // Number of registers needed for L2 propagation
-        constexpr size_t NUM_REGISTERS = L2_SIZE / CHUNK_SIZE_32BIT;
-        vepi32 l2_regs[NUM_REGISTERS];
+    // Number of registers needed for L2 propagation
+    constexpr size_t NUM_REGISTERS = L2_SIZE / CHUNK_SIZE_32BIT;
+    vepi32 l2_regs[NUM_REGISTERS];
 
-        // Init registers with L2 biases
-        for (size_t i = 0; i < NUM_REGISTERS; ++i) {
-            l2_regs[i] = load_i32(&network.l1_biases[bucket][i * CHUNK_SIZE_32BIT]);
-        }
+    // Init registers with L2 biases
+    for (size_t i = 0; i < NUM_REGISTERS; ++i) {
+        l2_regs[i] = load_i32(&network.l1_biases[bucket][i * CHUNK_SIZE_32BIT]);
+    }
 
-        // Accumulate dot products
-        for (size_t chunk_idx = 0; chunk_idx < L1_SIZE / 4; ++chunk_idx) {
-            vepi32 input = set_i32(packed_input[chunk_idx]);
-
-            for (size_t i = 0; i < NUM_REGISTERS; ++i) {
-                vepi8 weights = load_i8(&network.l1_weights[bucket][chunk_idx][i * CHUNK_SIZE_32BIT][0]);
-                l2_regs[i] = dpbusd_i32(l2_regs[i], input, weights);
-            }
-        }
-
-        // Apply shift, SCReLU activation and store
-        const vepi32 zero = zero_i32();
-        const vepi32 one = set_i32(QC);
+    // Accumulate dot products
+    for (size_t chunk_idx = 0; chunk_idx < L1_SIZE / 4; ++chunk_idx) {
+        vepi32 input = set_i32(packed_input[chunk_idx]);
 
         for (size_t i = 0; i < NUM_REGISTERS; ++i) {
-            vepi32 reg = shiftright_i32(l2_regs[i], shift);
-
-            reg = clamp_i32(reg, zero, one);
-            reg = mullo_i32(reg, reg);
-
-            store_i32(&outputs[i * CHUNK_SIZE_32BIT], reg);
-        }
-    } else {
-        // Initialize accumulators with biases
-        for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
-            outputs[output_idx] = network.l1_biases[bucket][output_idx];
-        }
-
-        // Accumulate dot products, simulating dpbusd_i32
-        for (size_t chunk = 0; chunk < L1_SIZE / 4; ++chunk) {
-            const int32_t in0 = inputs[chunk * 4 + 0];
-            const int32_t in1 = inputs[chunk * 4 + 1];
-            const int32_t in2 = inputs[chunk * 4 + 2];
-            const int32_t in3 = inputs[chunk * 4 + 3];
-
-            for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
-                const int32_t w0 = network.l1_weights[bucket][chunk][output_idx][0];
-                const int32_t w1 = network.l1_weights[bucket][chunk][output_idx][1];
-                const int32_t w2 = network.l1_weights[bucket][chunk][output_idx][2];
-                const int32_t w3 = network.l1_weights[bucket][chunk][output_idx][3];
-
-                outputs[output_idx] += (in0 * w0) + (in1 * w1) + (in2 * w2) + (in3 * w3);
-            }
-        }
-
-        // Apply shift, SCReLU activation and store
-        for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
-            int32_t v = outputs[output_idx] >> shift;
-            v = std::clamp(v, 0, QC);
-            outputs[output_idx] = v * v;
+            vepi8 weights = load_i8(&network.l1_weights[bucket][chunk_idx][i * CHUNK_SIZE_32BIT][0]);
+            l2_regs[i] = dpbusd_i32(l2_regs[i], input, weights);
         }
     }
+
+    // Apply shift, SCReLU activation and store
+    const vepi32 zero = zero_i32();
+    const vepi32 one = set_i32(QC);
+
+    for (size_t i = 0; i < NUM_REGISTERS; ++i) {
+        vepi32 reg = shiftright_i32(l2_regs[i], shift);
+
+        reg = clamp_i32(reg, zero, one);
+        reg = mullo_i32(reg, reg);
+
+        store_i32(&outputs[i * CHUNK_SIZE_32BIT], reg);
+    }
+#else
+    // Initialize accumulators with biases
+    for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
+        outputs[output_idx] = network.l1_biases[bucket][output_idx];
+    }
+
+    // Accumulate dot products, simulating dpbusd_i32
+    for (size_t chunk = 0; chunk < L1_SIZE / 4; ++chunk) {
+        const int32_t in0 = inputs[chunk * 4 + 0];
+        const int32_t in1 = inputs[chunk * 4 + 1];
+        const int32_t in2 = inputs[chunk * 4 + 2];
+        const int32_t in3 = inputs[chunk * 4 + 3];
+
+        for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
+            const int32_t w0 = network.l1_weights[bucket][chunk][output_idx][0];
+            const int32_t w1 = network.l1_weights[bucket][chunk][output_idx][1];
+            const int32_t w2 = network.l1_weights[bucket][chunk][output_idx][2];
+            const int32_t w3 = network.l1_weights[bucket][chunk][output_idx][3];
+
+            outputs[output_idx] += (in0 * w0) + (in1 * w1) + (in2 * w2) + (in3 * w3);
+        }
+    }
+
+    // Apply shift, SCReLU activation and store
+    for (size_t output_idx = 0; output_idx < L2_SIZE; ++output_idx) {
+        int32_t v = outputs[output_idx] >> shift;
+        v = std::clamp(v, 0, QC);
+        outputs[output_idx] = v * v;
+    }
+#endif
 }
 
 /// Does not activate outputs, that's done on 'propagate_l3'
@@ -231,58 +231,58 @@ void NNUE::propagate_l2(int bucket, std::span<const int32_t, L2_SIZE> inputs, st
     std::memcpy(outputs.data(), &network.l2_biases[bucket], outputs.size_bytes());
 
     // L2 matmul
-    if constexpr (USE_SIMD) {
-        using namespace simd;
+#if USE_SIMD
+    using namespace simd;
 
-        for (size_t input_idx = 0; input_idx < L2_SIZE; ++input_idx) {
-            vepi32 input = set_i32(inputs[input_idx]);
+    for (size_t input_idx = 0; input_idx < L2_SIZE; ++input_idx) {
+        vepi32 input = set_i32(inputs[input_idx]);
 
-            for (size_t output_idx = 0; output_idx < L3_SIZE; output_idx += CHUNK_SIZE_32BIT) {
-                vepi32 weights = load_i32(&network.l2_weights[bucket][input_idx][output_idx]);
-                vepi32 output = load_i32(&outputs[output_idx]);
+        for (size_t output_idx = 0; output_idx < L3_SIZE; output_idx += CHUNK_SIZE_32BIT) {
+            vepi32 weights = load_i32(&network.l2_weights[bucket][input_idx][output_idx]);
+            vepi32 output = load_i32(&outputs[output_idx]);
 
-                vepi32 product = mullo_i32(input, weights);
-                output = add_i32(output, product);
+            vepi32 product = mullo_i32(input, weights);
+            output = add_i32(output, product);
 
-                store_i32(&outputs[output_idx], output);
-            }
-        }
-    } else {
-        for (size_t input_idx = 0; input_idx < L2_SIZE; ++input_idx) {
-            const int32_t input = inputs[input_idx];
-            const int32_t *weights = &network.l2_weights[bucket][input_idx][0];
-            for (size_t output_idx = 0; output_idx < L3_SIZE; ++output_idx) {
-                outputs[output_idx] += weights[output_idx] * input;
-            }
+            store_i32(&outputs[output_idx], output);
         }
     }
+#else
+    for (size_t input_idx = 0; input_idx < L2_SIZE; ++input_idx) {
+        const int32_t input = inputs[input_idx];
+        const int32_t *weights = &network.l2_weights[bucket][input_idx][0];
+        for (size_t output_idx = 0; output_idx < L3_SIZE; ++output_idx) {
+            outputs[output_idx] += weights[output_idx] * input;
+        }
+    }
+#endif
 }
 
 void NNUE::propagate_l3(int bucket, std::span<const int32_t, L3_SIZE> inputs, int32_t &output) {
     // Activate L2 outputs ('inputs') and L3 matmul
-    if constexpr (USE_SIMD) {
-        using namespace simd;
+#if USE_SIMD
+    using namespace simd;
 
-        const vepi32 zero = zero_i32();
-        const vepi32 one = set_i32(QC * QC * QC);
+    const vepi32 zero = zero_i32();
+    const vepi32 one = set_i32(QC * QC * QC);
 
-        vepi32 output_vec = zero;
-        for (size_t idx = 0; idx < L3_SIZE; idx += CHUNK_SIZE_32BIT) {
-            vepi32 input = load_i32(&inputs[idx]);
-            vepi32 weights = load_i32(&network.l3_weights[bucket][idx]);
+    vepi32 output_vec = zero;
+    for (size_t idx = 0; idx < L3_SIZE; idx += CHUNK_SIZE_32BIT) {
+        vepi32 input = load_i32(&inputs[idx]);
+        vepi32 weights = load_i32(&network.l3_weights[bucket][idx]);
 
-            input = clamp_i32(input, zero, one); // Activate L2 outputs
-            output_vec = add_i32(output_vec, mullo_i32(input, weights));
-        }
-
-        output = hsum_i32(output_vec);
-    } else {
-        output = 0;
-        for (size_t idx = 0; idx < L3_SIZE; ++idx) {
-            const int32_t l2_out = std::clamp(inputs[idx], 0, QC * QC * QC); // Activate L2 output
-            output += l2_out * network.l3_weights[bucket][idx];
-        }
+        input = clamp_i32(input, zero, one); // Activate L2 outputs
+        output_vec = add_i32(output_vec, mullo_i32(input, weights));
     }
+
+    output = hsum_i32(output_vec);
+#else
+    output = 0;
+    for (size_t idx = 0; idx < L3_SIZE; ++idx) {
+        const int32_t l2_out = std::clamp(inputs[idx], 0, QC * QC * QC); // Activate L2 output
+        output += l2_out * network.l3_weights[bucket][idx];
+    }
+#endif
 
     // Add L3 bias
     output += network.l3_biases[bucket];
