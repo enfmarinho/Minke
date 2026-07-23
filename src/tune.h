15b01626009cc5e92a95621c27c0101a7daa68d3
enfmarinho <eduardonestormarinho228@gmail.com>
2026-07-23 02:13:11 -0300

    Add tunable parameters to NMP beta margin Bench: 4772706

--- src/tune.h
+++ src/tune.h
@@ -129,6 +129,9 @@ TUNABLE_PARAM(aw_widening_factor, 39, 1, 100, 5, 0.002)
 // Null move pruning
 TUNABLE_PARAM(nmp_base_reduction, 304, 128, 384, 15, 0.002)
 TUNABLE_PARAM(nmp_depth_factor, 21, 16, 63, 0.5, 0.002)
+TUNABLE_PARAM(nmp_beta_base_margin, 200, 10, 300, 20, 0.002)
+TUNABLE_PARAM(nmp_beta_improving_margin, 50, 0, 100, 5, 0.002)
+TUNABLE_PARAM(nmp_beta_depth_factor, 1300, 500, 2000, 50, 0.002)
 FIXED_PARAM(nmp_min_depth, 2, 2, 8, 0.5, 0.002)
 
 // Hindsight
