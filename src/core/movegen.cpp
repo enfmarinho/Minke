c8488fa36ad2800cd9eacfd15e33d2ecc59d4b1e
enfmarinho <eduardonestormarinho228@gmail.com>
2026-08-10 19:08:26 -0300

    Optimize noisy pawns movegen

--- src/core/movegen.cpp
+++ src/core/movegen.cpp
@@ -61,44 +61,84 @@ static inline void push_pawn_promotions(ScoredMoveList& move_list, const Directi
 
 static inline void gen_pawn_noisies(ScoredMoveList& move_list, const Position& pos, Bitboard dst_mask) {
     const Color stm = pos.stm();
+    const Color nstm = pos.nstm();
     const Direction push = get_pawn_offset(stm);
-    const Direction west_push = static_cast<Direction>(WEST + push);
-    const Direction east_push = static_cast<Direction>(EAST + push);
+    const Direction west_capture = static_cast<Direction>(WEST + push);
+    const Direction east_capture = static_cast<Direction>(EAST + push);
 
-    const Bitboard pawn_promotion_rank = Bitboard::pawn_promotion_rank(stm);
+    const Bitboard pawns_bb = pos.piece_bb(PAWN, pos.stm());
     const Bitboard theirs = pos.occ_bb(pos.nstm());
+    const Bitboard pawn_promotion_rank = Bitboard::pawn_promotion_rank(stm);
 
-    Bitboard pawns_bb = pos.piece_bb(PAWN, pos.stm());
+    // pins bitboards
+    const Square king_sq = pos.king_sq(stm);
+    const Bitboard pins_bb = pos.pins_bb();
+    const Bitboard west_capture_pin_mask = stm == WHITE ? antidiagonal_masks[king_sq] : diagonal_masks[king_sq];
+    const Bitboard east_capture_pin_mask = stm == WHITE ? diagonal_masks[king_sq] : antidiagonal_masks[king_sq];
 
-    Bitboard west_captures_bb = pawns_bb.shift_up_west_pov(stm) & dst_mask;
-    Bitboard east_captures_bb = pawns_bb.shift_up_east_pov(stm) & dst_mask;
+    // captures destination bitboards
+    const Bitboard west_attackers_bb = (pawns_bb & ~pins_bb) | (pawns_bb & west_capture_pin_mask);
+    const Bitboard west_captures_bb = west_attackers_bb.shift_up_west_pov(stm) & dst_mask & theirs;
+
+    const Bitboard east_attackers_bb = (pawns_bb & ~pins_bb) | (pawns_bb & east_capture_pin_mask);
+    const Bitboard east_captures_bb = east_attackers_bb.shift_up_east_pov(stm) & dst_mask & theirs;
 
     // Promotions captures
-    Bitboard west_captures_promos_bb = west_captures_bb & pawn_promotion_rank & theirs;
-    Bitboard east_captures_promos_bb = east_captures_bb & pawn_promotion_rank & theirs;
-    push_pawn_promotions(move_list, west_push, west_captures_promos_bb, true);
-    push_pawn_promotions(move_list, east_push, east_captures_promos_bb, true);
+    const Bitboard west_captures_promos_bb = west_captures_bb & pawn_promotion_rank;
+    const Bitboard east_captures_promos_bb = east_captures_bb & pawn_promotion_rank;
+    push_pawn_promotions(move_list, west_capture, west_captures_promos_bb, true);
+    push_pawn_promotions(move_list, east_capture, east_captures_promos_bb, true);
 
     // Promotions
-    Bitboard pawn_promos_bb = pawns_bb.shift_up_pov(stm) & dst_mask & ~theirs;
+    const Bitboard forward_pin_mask = Bitboard::file(king_sq);
+    const Bitboard pawn_possible_promos_bb = (pawns_bb & ~pins_bb) | (pawns_bb & forward_pin_mask);
+    const Bitboard pawn_promos_bb = pawn_possible_promos_bb.shift_up_pov(stm) & dst_mask & ~theirs;
     push_pawn_promotions(move_list, push, pawn_promos_bb, false);
 
     // Captures that aren't promotions
-    Bitboard west_captures_not_promos_bb = west_captures_bb & ~west_captures_promos_bb & theirs;
-    Bitboard east_captures_not_promos_bb = east_captures_bb & ~east_captures_promos_bb & theirs;
-    push_pawn_moves<CAPTURE>(move_list, west_push, west_captures_not_promos_bb);
-    push_pawn_moves<CAPTURE>(move_list, east_push, east_captures_not_promos_bb);
+    const Bitboard west_captures_not_promos_bb = west_captures_bb & ~west_captures_promos_bb;
+    const Bitboard east_captures_not_promos_bb = east_captures_bb & ~east_captures_promos_bb;
+    push_pawn_moves<CAPTURE>(move_list, west_capture, west_captures_not_promos_bb);
+    push_pawn_moves<CAPTURE>(move_list, east_capture, east_captures_not_promos_bb);
 
     // En-passant
-    Square ep_sq = pos.ep_sq();
+    const Square ep_sq = pos.ep_sq();
     if (ep_sq != NO_SQ) {
-        Bitboard ep_sq_mask(ep_sq);
+        const Bitboard ep_sq_mask(ep_sq);
 
-        Bitboard west_attacker_bb = pawns_bb.shift_up_west_pov(stm) & ep_sq_mask;
-        Bitboard east_attacker_bb = pawns_bb.shift_up_east_pov(stm) & ep_sq_mask;
+        Bitboard ep_west_captures_bb = west_attackers_bb.shift_up_west_pov(stm) & ep_sq_mask;
+        Bitboard ep_east_captures_bb = east_attackers_bb.shift_up_east_pov(stm) & ep_sq_mask;
+        const Square captured_pawn_sq = static_cast<Square>(ep_sq - static_cast<int>(push));
 
-        push_pawn_moves<EP>(move_list, west_push, west_attacker_bb);
-        push_pawn_moves<EP>(move_list, east_push, east_attacker_bb);
+        if (pos.in_check()) {
+            const bool captures_checker = pos.checkers_bb().is_set(captured_pawn_sq);
+            const bool blocks_check = inbetween_masks[king_sq][pos.checkers_bb().lsb()].is_set(ep_sq);
+
+            if (!captures_checker && !blocks_check) {
+                ep_west_captures_bb = 0;
+                ep_east_captures_bb = 0;
+            }
+        }
+
+        auto ep_discovers_check = [&](Direction capture_offset, Bitboard ep_attacker_bb) -> bool {
+            assert(ep_attacker_bb.popcount() <= 1); // at most one pawn can land on ep_sq from a given direction
+
+            if (!ep_attacker_bb)
+                return false;
+
+            const Square from_sq = static_cast<Square>(ep_attacker_bb.lsb() - static_cast<int>(capture_offset));
+            const Bitboard occ_no_moved_bb = pos.occ_bb() ^ Bitboard(from_sq) ^ Bitboard(captured_pawn_sq);
+            const Bitboard horizontal_attackers = pos.piece_bb(ROOK, nstm) | pos.piece_bb(QUEEN, nstm);
+
+            return static_cast<bool>(get_rook_attacks(king_sq, occ_no_moved_bb) & horizontal_attackers &
+                                     Bitboard::rank(king_sq));
+        };
+
+        if (!ep_discovers_check(west_capture, ep_west_captures_bb))
+            push_pawn_moves<EP>(move_list, west_capture, ep_west_captures_bb);
+
+        if (!ep_discovers_check(east_capture, ep_east_captures_bb))
+            push_pawn_moves<EP>(move_list, east_capture, ep_east_captures_bb);
     }
 }
 
