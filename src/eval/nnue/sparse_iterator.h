abe6b075396afd1863d77bc8b0b9955e0aa4ecde
Eduardo Marinho <eduardomarinho@pm.me>
2026-08-26 11:00:52 -0300

    perf(eval): optimize sparse iterator for AVX512 (#204)
    Bench: 6748220

--- src/eval/nnue/sparse_iterator.h
+++ src/eval/nnue/sparse_iterator.h
@@ -18,10 +18,13 @@
 
 #pragma once
 
-#include <bit>
-#include <limits>
-
 #if USE_SIMD
+
+#if USE_NEON
+#include <arm_neon.h>
+#else
+#include <immintrin.h>
+#endif
 
 #include <cassert>
 #include <cstddef>
@@ -35,69 +38,31 @@ class SparseIterator {
     inline size_t count() const { return m_count; }
     inline uint16_t chunk(size_t nnz_id) const { return m_chunks[nnz_id]; }
 
-    void update(simd::vepu8 a, simd::vepu8 b) {
-        using namespace simd;
-        const uint32_t full_mask = (nonzero_mask_u8(b) << CHUNK_SIZE_32BIT) | nonzero_mask_u8(a);
-
-        for (uint32_t out = 0; out < CHUNK_SIZE_32BIT / 4; ++out) {
-            const uint8_t mask = (full_mask >> (out * 8)) & 0xFF;
-
-            const vep128u16 nonzero = load(&nonzero_idx[mask]);
-            const vep128u16 idxs = add(m_base, nonzero);
-            ustore(&m_chunks[m_count], idxs);
-
-            m_base = add(m_base, set1(8));
-            m_count += std::popcount(mask);
-        }
-
-        assert(m_count <= L1_SIZE / 4);
-    }
+    void update(simd::vepu8 a, simd::vepu8 b);
 
   private:
-    alignas(16) static constexpr auto nonzero_idx = []() {
-        constexpr size_t COUNT = std::numeric_limits<uint8_t>::max() + 1;
-
-        std::array<std::array<uint16_t, 8>, COUNT> idx{};
-
-        for (size_t i = 0; i < COUNT; ++i) {
-            size_t count = 0;
-
-            for (uint8_t mask = i; mask != 0; mask &= mask - 1) {
-                idx[i][count++] = std::countr_zero(mask);
-            }
-        }
-
-        return idx;
-    }();
-
 #if USE_NEON
-    using vep128u16 = uint16x8_t;
-
-    inline static vep128u16 zero() { return vdupq_n_u16(0); }
-
-    inline static vep128u16 set1(uint16_t v) { return vdupq_n_u16(v); }
-
-    inline static vep128u16 load(const void* ptr) { return vld1q_u16(reinterpret_cast<const uint16_t*>(ptr)); }
-
-    inline static void ustore(void* ptr, vep128u16 v) { return vst1q_u16(reinterpret_cast<uint16_t*>(ptr), v); }
-
-    inline static vep128u16 add(vep128u16 a, vep128u16 b) { return vaddq_u16(a, b); }
+    using vepu16 = uint16x8_t;
+    static inline vepu16 init_base() { return vdupq_n_u16(0); }
+#elif USE_AVX2
+    using vepu16 = __m128i;
+    static inline vepu16 init_base() { return _mm_setzero_si128(); }
+#elif USE_AVX512
+    using vepu16 = __m512i;
+    static inline vepu16 init_base() {
+        return _mm512_set_epi16(            //
+            31, 30, 29, 28, 27, 26, 25, 24, //
+            23, 22, 21, 20, 19, 18, 17,     //
+            16, 15, 14, 13, 12, 11, 10,     //
+            9, 8, 7, 6, 5, 4, 3, 2, 1, 0    //
+        );
+    }
 #else
-    using vep128u16 = __m128i;
-
-    inline static vep128u16 zero() { return _mm_setzero_si128(); }
-
-    inline static vep128u16 set1(uint16_t v) { return _mm_set1_epi16(static_cast<int16_t>(v)); }
-
-    inline static vep128u16 load(const void* ptr) { return _mm_load_si128(static_cast<const __m128i*>(ptr)); }
-
-    inline static void ustore(void* ptr, vep128u16 v) { _mm_storeu_si128(static_cast<__m128i*>(ptr), v); }
-
-    inline static vep128u16 add(vep128u16 a, vep128u16 b) { return _mm_add_epi16(a, b); }
+#error "USE_SIMD is enabled, but no supported SIMD architecture is selected"
 #endif
 
-    alignas(16) uint16_t m_chunks[L1_SIZE / 4] = {};
-    vep128u16 m_base = zero();
+    alignas(64) uint16_t m_chunks[L1_SIZE / 4] = {};
+    vepu16 m_base = init_base();
     size_t m_count = 0;
 };
 
